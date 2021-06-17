a0b126350fa29b0513a10d40f899b743fa0cc823
BaoPQ19 <baopq19@gmail.com>
2021-06-17 17:32:19 +0700

    2st commit_Load table employee_add employee function with validated data

---
+++ js/model/employee.js
@@ -0,0 +1,69 @@
+class Employee {
+    constructor(id, name, position, salary, hours) {
+        this.id = Number(id);
+        this.name = name;
+        this.position = position;
+        this.salary = Number(salary);
+        this.hours = Number(hours);
+    }
+
+    calculateTotalSalary() {
+        let totalSalary = this.salary;
+        if(this.position === 'Quản lý') {
+            totalSalary *= 2;
+        } else if(this.position === 'Giám đốc') {
+            totalSalary *= 3;
+        }
+        return totalSalary;
+    }
+
+    ranking() {
+        let rank = 'Nhân viên trung bình';
+        if(this.hours >= 80) {
+            rank = 'Nhân viên khá'
+        } else if(this.hours >= 100) {
+            rank = 'Nhân viên giỏi';
+        } else if(this.hours >= 120) {
+            rank = 'Nhân viên xuất sắc'
+        }
+        return rank;
+    }
+
+    getId() {
+        return this.id;
+    }
+
+    getName() {
+        return this.name;
+    }
+
+    setName(name) {
+        this.name = name;
+    }
+
+    getPosition() {
+        return this.position;
+    }
+
+    setPosition(position) {
+        this.position = position;
+    }
+
+    getSalary() {
+        return this.salary;
+    }
+
+    setSalary(salary) {
+        this.salary = salary;
+    }
+
+    getHours() {
+        return this.hours;
+    }
+
+    setHours(hours) {
+        this.hours = hours;
+    }
+
+    
+}
---
+++ js/script.js
@@ -0,0 +1,176 @@
+const apiURL = 'http://svcy.myclass.vn/api/QuanLyNhanVienApi'
+
+// Get elements
+const inpId = document.querySelector('#inpId');
+const inpIdError = document.querySelector('#inpIdError');
+const inpName = document.querySelector('#inpName');
+const inpNameError = document.querySelector('#inpNameError');
+const selPosition = document.querySelector('#selPosition');
+const inpSalary = document.querySelector('#inpSalary');
+const inpSalaryError = document.querySelector('#inpSalaryError');
+const inpHours = document.querySelector('#inpHours');
+const inpHoursError = document.querySelector('#inpHoursError');
+const btnAdd = document.querySelector('#btnAdd');
+const btnUpdate = document.querySelector('#btnUpdate');
+const tbManage = document.querySelector('#tbManage');
+
+btnUpdate.disabled = true;
+
+var employees = new Array;
+
+window.onload = (event) => {
+    getEmployees();
+};
+
+btnAdd.addEventListener('click', event => {
+    event.preventDefault();
+    let employee = getFormData();
+    let valid = validation(employee);
+    if(valid) {
+        addEmployee(employee);
+    }
+});
+
+const getFormData = function() {
+    const employee = new Employee(
+        inpId.value,
+        inpName.value,
+        selPosition.value,
+        inpSalary.value,
+        inpHours.value
+    )
+    return employee;
+};
+
+const validation = function(employee) {
+    let valid = true;
+
+    if(employee.id != "") {
+        if(employee.id < 1 || employee.id > 999999) {
+            inpIdError.innerHTML = '(Mã số nhân viên phải lớn hơn 0 và nhỏ hơn 999999)'
+            valid = false;
+        } else {
+            inpIdError.innerHTML = '';
+        }
+    } else {
+        valid = false;
+        inpIdError.innerHTML = '(Mã số nhân viên không được trống)'
+    }
+
+    if(employee.name != "") {
+        let regex = new RegExp('^[a-zA-ZÀÁÂÃÈÉÊÌÍÒÓÔÕÙÚĂĐĨŨƠàáâãèéêìíòóôõùúăđĩũơƯĂẠẢẤẦẨẪẬẮẰẲẴẶẸẺẼỀỀỂẾưăạảấầẩẫậắằẳẵặẹẻẽềềểếỄỆỈỊỌỎỐỒỔỖỘỚỜỞỠỢỤỦỨỪễệỉịọỏốồổỗộớờởỡợụủứừỬỮỰỲỴÝỶỸửữựỳỵỷỹ\s\W|_ ]+$');
+        if(!regex.test(employee.name)) {
+            inpNameError.innerHTML = '(Tên nhân viên không được chứa số hoặc kí tự đặc biệt)'
+            valid = false;
+        } else {
+            inpNameError.innerHTML = '';
+        }
+    } else {
+        valid = false;
+        inpNameError.innerHTML = '(Tên nhân viên không được trống)'
+    }
+
+    if(employee.salary != "") {
+        if(employee.salary < 1000000 || employee.salary > 20000000) {
+            inpSalaryError.innerHTML = '(Lương cơ bản nằm trong mức 1 000 000 - 20 000 000)'
+            valid = false;
+        } else {
+            inpSalaryError.innerHTML = '';
+        }
+    } else {
+        valid = false;
+        inpSalaryError.innerHTML = '(Lương cơ bản không được trống)'
+    }
+
+    if(employee.hours != "") {
+        if(employee.hours < 50 || employee.hours > 150) {
+            inpHoursError.innerHTML = '(Số giờ làm nằm trong mức 50 - 150)'
+            valid = false;
+        } else {
+            inpHoursError.innerHTML = '';
+        }
+    } else {
+        valid = false;
+        inpHoursError.innerHTML = '(Số giờ làm không được trống)'
+    }
+    
+    return valid;
+};
+
+const getEmployees = function() {
+    employees = new Array;
+    axios.get(`${apiURL}/LayDanhSachNhanVien`)
+        .then(function(response) {
+            response.data.forEach(e => {
+                let employee = new Employee(
+                    e.maNhanVien,
+                    e.tenNhanVien,
+                    e.chucVu,
+                    e.luongCoBan,
+                    e.soGioLamTrongThang
+                );
+                employees.push(employee);
+            })
+            loadTable(employees);
+        })
+        .catch(function(error) {
+            console.log(error)
+        })
+};
+
+const loadTable = function(employees) {
+    let bodyValue = '';
+    employees.forEach(employee => {
+        bodyValue += `
+        <tr>
+            <td>${employee.id}</td>
+            <td>${employee.name}</td>
+            <td>${employee.position}</td>
+            <td>${employee.salary}</td>
+            <td>${employee.calculateTotalSalary()}</td>
+            <td>${employee.hours}</td>
+            <td>${employee.ranking()}</td>
+            <td>
+                <button class="btn btn-danger" onclick="deleteEmployee('${employee.id}')">Xoá</button>
+                <button class="btn btn-outline-primary" onclick="editEmployee('${employee.id}')">Chỉnh sửa</button>
+            </td>
+        </tr>`;
+    });
+    tbManage.innerHTML = bodyValue;
+};
+
+const addEmployee = function(employee) {
+    let nhanVien = parseNhanVien(employee);
+    let promise = axios({
+        method: 'post',
+        url: `${apiURL}/ThemNhanVien`,
+        data: nhanVien
+    })
+
+    promise.then(function(response) {
+        getEmployees();
+    })
+
+    promise.catch(function(error) {
+        console.log(error)
+    })
+};
+
+const parseNhanVien = function(employee) {
+    let hscv = 1;
+    if(employee.position === 'Quản lý') {
+        hscv = 2;
+    } else if(hscv === 'Giám đốc'){
+        hscv = 3;
+    }
+    let nhanVien = {
+        "maNhanVien": employee.id,
+        "tenNhanVien": employee.name,
+        "chucVu": employee.position,
+        "heSoChucVu": hscv,
+        "luongCoBan": employee.salary,
+        "soGioLamTrongThang": employee.hours
+    }
+
+    return nhanVien;
+}
